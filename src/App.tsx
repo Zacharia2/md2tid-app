52a35e5543c0b5f8b4db88cb836e5a53aa5163c6
XINGLAN <32425955+Zacharia2@users.noreply.github.com>
2025-03-22 09:02:43 +0800

    fix ast and update depts

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ import {
   Tab,
   Tabs,
   TextField,
+  Theme,
   Typography,
   useTheme,
 } from "@mui/material";
@@ -28,49 +29,8 @@ import { FixedSizeList as List, ListChildComponentProps } from "react-window";
 function App() {
   // let dir = "C:/Users/Snowy/Documents/GitHub/md2tid/src";
 
-  const [dirs, setDirs] = useState<string[]>([]);
-
-  const [ensurepath, setEnsurePath] = useState("");
-  const [targetPath, setTargetPath] = useState("");
-
-  const [isLoading, setIsLoading] = useState(false);
-
   const [tabvalue, setTabValue] = useState(1);
   const theme = useTheme();
-
-  const [multilineIn, setMultilineIn] = useState("");
-  const [multilineOut, setMultilineOut] = useState("");
-  const multilineRef = useRef("");
-
-  // 变量、引用、状态
-  // remove(testwfile);
-  function renderRow(props: ListChildComponentProps) {
-    const { index, style } = props;
-    let value = dirs[index];
-    let rel_path = relpath(value, ensurepath);
-    return (
-      <ListItem
-        style={style}
-        component="div"
-        key={value}
-        disableGutters
-        secondaryAction={
-          <ListItemButton
-            aria-label="deleteItem"
-            onClick={() =>
-              setDirs((currentDirs) =>
-                currentDirs.filter((dir) => dir !== value)
-              )
-            }
-          >
-            <DeleteOutline />
-          </ListItemButton>
-        }
-      >
-        <ListItemText primary={`VFile: ${rel_path}`} />
-      </ListItem>
-    );
-  }
 
   const [open, setOpen] = useState(false);
   const [message, setMessage] = useState("");
@@ -106,145 +66,24 @@ function App() {
         <Tab value={1} label="批量转换" />
         <Tab value={2} label="设置" />
       </Tabs>
-      <TabPanel value={tabvalue} index={0} dir={theme.direction}>
-        <Box className="container">
-          <TextField
-            id="outlined-multiline-static"
-            label="输入"
-            multiline
-            rows={8}
-            autoFocus
-            onFocus={(e) => {
-              e.currentTarget.setSelectionRange(
-                e.currentTarget.value.length,
-                e.currentTarget.value.length
-              );
-            }}
-            value={multilineIn}
-            onChange={(event) => {
-              multilineRef.current = event.target.value;
-              setMultilineIn(event.target.value);
-              md2tid(multilineRef.current).then((data) => {
-                setMultilineOut(data);
-              });
-            }}
-          />
-          <br></br>
-          <TextField
-            id="outlined-multiline-static"
-            label="输出"
-            multiline
-            rows={8}
-            value={multilineOut}
-          />
-        </Box>
-      </TabPanel>
-      <TabPanel value={tabvalue} index={1} dir={theme.direction}>
-        <Box className="container">
-          <TextField
-            id="outlined-basic"
-            label="输入"
-            variant="outlined"
-            size="small"
-            style={{ marginBottom: 10 }}
-            value={ensurepath}
-            onChange={(e) => {
-              setEnsurePath(e.target.value);
-            }}
-          />
-          <TextField
-            id="outlined-basic"
-            label="输出"
-            variant="outlined"
-            size="small"
-            style={{ marginBottom: 10 }}
-            value={targetPath}
-            onChange={(e) => {
-              setTargetPath(e.target.value);
-            }}
-          />
-          <Button
-            variant="outlined"
-            style={{ marginBottom: 10 }}
-            onClick={() => {
-              if (ensurepath == "") {
-                setOpen(true);
-                setMessage("无效路径");
-                return;
-              }
-              setIsLoading(true);
-              readMdInDirs(ensurepath).then((dirs) => {
-                setDirs(dirs);
-                setIsLoading(false);
-              });
-            }}
-          >
-            读取文件列表
-          </Button>
-          {isLoading ? (
-            <Box
-              sx={{
-                height: 300,
-                borderRadius: 1,
-              }}
-              display="flex"
-              justifyContent="center" // 水平居中
-              alignItems="center" // 垂直居中
-            >
-              <CircularProgress style={{}} />
-            </Box>
-          ) : (
-            <List
-              height={300}
-              width={"100%"}
-              itemSize={30}
-              itemCount={dirs.length}
-              overscanCount={5}
-              style={{ maxHeight: 400 }}
-            >
-              {renderRow}
-            </List>
-          )}
-
-          <Button
-            variant="outlined"
-            onClick={() => {
-              if (dirs.length == 0) {
-                setOpen(true);
-                setMessage("无可处理内容");
-                return;
-              } else if (targetPath == "") {
-                setOpen(true);
-                setMessage("目标路径为空");
-                return;
-              }
-              setOpen(true);
-              setMessage("正在处理");
-              for (let index = 0; index < dirs.length; index++) {
-                const element = dirs[index];
-                transform(ensurepath, element, targetPath);
-              }
-            }}
-          >
-            开始转换文件
-          </Button>
-        </Box>
-      </TabPanel>
-      <TabPanel value={tabvalue} index={2} dir={theme.direction}>
-        项目地址: <a>https://github.com/Zacharia2/md2tid</a>
-      </TabPanel>
+      <动态转换 tabvalue={tabvalue} theme={theme}></动态转换>
+      <批量转换
+        tabvalue={tabvalue}
+        theme={theme}
+        setOpen={setOpen}
+        setMessage={setMessage}
+      ></批量转换>
+      <设置 tabvalue={tabvalue} theme={theme}></设置>
     </>
   );
 }
 
-interface TabPanelProps {
+function TabPanel(props: {
   children?: React.ReactNode;
   dir?: string;
   index: number;
   value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
+}) {
   const { children, value, index, ...other } = props;
 
   return (
@@ -261,6 +100,187 @@ function TabPanel(props: TabPanelProps) {
         </Box>
       )}
     </div>
+  );
+}
+function 动态转换(props: { tabvalue: number; theme: Theme }) {
+  const [multilineIn, setMultilineIn] = useState("");
+  const [multilineOut, setMultilineOut] = useState("");
+  const multilineRef = useRef("");
+
+  return (
+    <TabPanel value={props.tabvalue} index={0} dir={props.theme.direction}>
+      <Box className="container">
+        <TextField
+          id="outlined-multiline-static"
+          label="输入"
+          multiline
+          rows={8}
+          autoFocus
+          onFocus={(e) => {
+            e.currentTarget.setSelectionRange(
+              e.currentTarget.value.length,
+              e.currentTarget.value.length
+            );
+          }}
+          value={multilineIn}
+          onChange={(event) => {
+            multilineRef.current = event.target.value;
+            setMultilineIn(event.target.value);
+            setMultilineOut(md2tid(multilineRef.current));
+          }}
+        />
+        <br></br>
+        <TextField
+          id="outlined-multiline-static"
+          label="输出"
+          multiline
+          rows={8}
+          value={multilineOut}
+        />
+      </Box>
+    </TabPanel>
+  );
+}
+function 批量转换(props: {
+  tabvalue: number;
+  theme: Theme;
+  setOpen: Function;
+  setMessage: Function;
+}) {
+  const [ensurepath, setEnsurePath] = useState("");
+  const [targetPath, setTargetPath] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [dirs, setDirs] = useState<string[]>([]);
+
+  // 变量、引用、状态
+  // remove(testwfile);
+  function renderRow(props: ListChildComponentProps) {
+    const { index, style } = props;
+    let value = dirs[index];
+    let rel_path = relpath(value, ensurepath);
+    return (
+      <ListItem
+        style={style}
+        component="div"
+        key={value}
+        disableGutters
+        secondaryAction={
+          <ListItemButton
+            aria-label="deleteItem"
+            onClick={() =>
+              setDirs((currentDirs) =>
+                currentDirs.filter((dir) => dir !== value)
+              )
+            }
+          >
+            <DeleteOutline />
+          </ListItemButton>
+        }
+      >
+        <ListItemText primary={`VFile: ${rel_path}`} />
+      </ListItem>
+    );
+  }
+  return (
+    <TabPanel value={props.tabvalue} index={1} dir={props.theme.direction}>
+      <Box className="container">
+        <TextField
+          id="outlined-basic"
+          label="输入"
+          variant="outlined"
+          size="small"
+          style={{ marginBottom: 10 }}
+          value={ensurepath}
+          onChange={(e) => {
+            setEnsurePath(e.target.value);
+          }}
+        />
+        <TextField
+          id="outlined-basic"
+          label="输出"
+          variant="outlined"
+          size="small"
+          style={{ marginBottom: 10 }}
+          value={targetPath}
+          onChange={(e) => {
+            setTargetPath(e.target.value);
+          }}
+        />
+        <Button
+          variant="outlined"
+          style={{ marginBottom: 10 }}
+          onClick={() => {
+            if (ensurepath == "") {
+              props.setOpen(true);
+              props.setMessage("无效路径");
+              return;
+            }
+            setIsLoading(true);
+            readMdInDirs(ensurepath).then((dirs) => {
+              setDirs(dirs);
+              setIsLoading(false);
+            });
+          }}
+        >
+          读取文件列表
+        </Button>
+        {isLoading ? (
+          <Box
+            sx={{
+              height: 300,
+              borderRadius: 1,
+            }}
+            display="flex"
+            justifyContent="center" // 水平居中
+            alignItems="center" // 垂直居中
+          >
+            <CircularProgress style={{}} />
+          </Box>
+        ) : (
+          <List
+            height={300}
+            width={"100%"}
+            itemSize={30}
+            itemCount={dirs.length}
+            overscanCount={5}
+            style={{ maxHeight: 400 }}
+          >
+            {renderRow}
+          </List>
+        )}
+
+        <Button
+          variant="outlined"
+          onClick={() => {
+            if (dirs.length == 0) {
+              props.setOpen(true);
+              props.setMessage("无可处理内容");
+              return;
+            } else if (targetPath == "") {
+              props.setOpen(true);
+              props.setMessage("目标路径为空");
+              return;
+            }
+            props.setOpen(true);
+            props.setMessage("正在处理");
+            for (let index = 0; index < dirs.length; index++) {
+              const element = dirs[index];
+              transform(ensurepath, element, targetPath);
+            }
+          }}
+        >
+          开始转换文件
+        </Button>
+      </Box>
+    </TabPanel>
+  );
+}
+
+function 设置(props: { tabvalue: number; theme: Theme }) {
+  return (
+    <TabPanel value={props.tabvalue} index={2} dir={props.theme.direction}>
+      项目地址: <a>https://github.com/Zacharia2/md2tid</a>
+    </TabPanel>
   );
 }
 
